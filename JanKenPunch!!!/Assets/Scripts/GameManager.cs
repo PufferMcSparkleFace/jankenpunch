889200fdecc49aceb5cd1048a8a9289525e994aa
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2025-03-26 03:53:47 +1300

    Characters turn around

--- JanKenPunch!!!/Assets/Scripts/GameManager.cs
+++ JanKenPunch!!!/Assets/Scripts/GameManager.cs
@@ -6,8 +6,6 @@ public class GameManager : MonoBehaviour
 {
     public Transform playArea;
     public GameObject lockInButton;
-    public GameObject player1;
-    public GameObject player2;
 
     // Start is called before the first frame update
     void Start()
