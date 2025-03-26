da8cab1d567c94e9e1efe63a72c8ad063c7d1f87
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2025-03-27 01:00:07 +1300

    slightly changing grab in the database

--- JanKenPunch!!!/Assets/Scripts/CardDatabase.cs
+++ JanKenPunch!!!/Assets/Scripts/CardDatabase.cs
@@ -12,7 +12,7 @@ public class CardDatabase : MonoBehaviour
         cardList.Add(new NonBasicCard(0, "Jab", 2, "Attack", "Strike", 1, "Mid", "", 1, 1, 0, -1));
         cardList.Add(new NonBasicCard(1, "Crouching Medium Kick", 3, "Attack", "Strike", 2, "Low", "", 2, 2, -2, -2));
         cardList.Add(new NonBasicCard(2, "Collarbone Breaker", 3, "Attack", "Strike", 1, "High", "", 3, 2, -2, -2));
-        cardList.Add(new NonBasicCard(3, "Grab", 3, "Attack", "Throw", 1, "", "Either push the target back 1 unit or swap spaces with them.", 5, 1, 100, -3));
+        cardList.Add(new NonBasicCard(3, "Grab", 3, "Attack", "Throw", 1, "", "Either push the target back 1 unit or swap spaces with them.", 5, 1, 0, -3));
         cardList.Add(new NonBasicCard(4, "Fireball", 4, "Attack", "Projectile", 4, "Mid", "Push the target back 1 unit.", 2, 0, -2, -5));
         cardList.Add(new NonBasicCard(5, "Dragon Uppercut", 2, "Attack", "Strike", 1, "Mid", "Push the target back 3 units.", 3, 1, -5, -5));
         cardList.Add(new NonBasicCard(6, "Hurricane Kick", 4, "Attack", "Strike", 1, "Mid", "Airborne, Move forward 2 units.", 1, 3, -3, -4));
