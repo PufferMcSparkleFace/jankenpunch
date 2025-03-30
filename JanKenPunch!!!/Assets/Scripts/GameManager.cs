ba6f2349f2585d5d2f3097eb512979073f65cd49
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2025-03-31 00:39:37 +1300

    Fixed annoying editor errors

--- JanKenPunch!!!/Assets/Scripts/GameManager.cs
+++ JanKenPunch!!!/Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public Transform playArea;
     public GameObject lockInButton;
-    public GameObject playedCard = null;
+    public GameObject playedCard;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +17,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playArea.childCount == 0)
-        {
-            playedCard.transform.localScale = new Vector3(2.5f, 3.5f, 0);
-            lockInButton.SetActive(false);
-            playedCard = null;
-        }
-        else
-        {
-            lockInButton.SetActive(true);
-            playedCard = playArea.GetChild(0).gameObject;
-            playedCard.transform.localScale = new Vector3(5, 7, 0);
-        }
+            if (playArea.childCount == 0)
+            {
+                if(playedCard != null)
+            {
+                playedCard.transform.localScale = new Vector3(2.5f, 3.5f, 0);
+                playedCard = null;
+            }                 
+                lockInButton.SetActive(false);           
+            }
+            else
+            {
+                lockInButton.SetActive(true);
+                playedCard = playArea.GetChild(0).gameObject;
+                playedCard.transform.localScale = new Vector3(5, 7, 0);
+            }
+        
     }
 
     public void LockIn()
