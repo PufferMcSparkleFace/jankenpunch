13db80a04b618c448b51572cb6770c3c315c3792
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2025-10-16 17:58:49 +1300

    it works i'm so fuckin smartttttttt

--- JanKenPunch!!!/Assets/Scripts/Functionality/Player.cs
+++ JanKenPunch!!!/Assets/Scripts/Functionality/Player.cs
@@ -400,34 +400,7 @@ public class Player : NetworkBehaviour
             }
             else if (playedCard.subtype == "Strike" && opponent.playedCard.subtype == "Strike")
             {
-                Debug.Log("Double Whiff");
-                opponent.energy = opponent.energy - opponent.finalCardCost;
-                opponent.energyText.text = "" + opponent.energy;
-                plusFrames = plusFrames + Mathf.Abs(opponent.playedCard.onWhiff);
-                opponent.plusFrames = opponent.plusFrames + Mathf.Abs(playedCard.onWhiff);
-                cumulativePlusFrames = plusFrames - opponent.plusFrames;
-                if (cumulativePlusFrames > 0)
-                {
-                    plusFrames = cumulativePlusFrames;
-                    plusFramesText.text = "+" + plusFrames;
-                    opponent.plusFrames = 0;
-                    opponent.plusFramesText.text = "";
-
-                }
-                else if (cumulativePlusFrames < 0)
-                {
-                    opponent.plusFrames = Mathf.Abs(cumulativePlusFrames);
-                    opponent.plusFramesText.text = "+" + opponent.plusFrames;
-                    plusFrames = 0;
-                    plusFramesText.text = "";
-                }
-                else if (cumulativePlusFrames == 0)
-                {
-                    plusFrames = cumulativePlusFrames;
-                    plusFramesText.text = "" + plusFrames;
-                    opponent.plusFrames = 0;
-                    opponent.plusFramesText.text = "" + opponent.plusFrames;
-                }
+                Debug.Log("Fixing Interaction...");
                 opponent.wereDone = true;
                 return;
             }
